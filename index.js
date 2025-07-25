0231cafc36638ea81910c8571b4c863263af342d
ponmana selvan <selvasa2277@gmail.com>
2025-07-25 12:53:03 +0530

    update cors

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ connectDB()
     .then(() => { console.log("Database Connected Successfully") })
     .catch((error) => { console.log(error, 'Database error') })
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: 'https://attendance-git-dev-02-timestamp-selvans-projects-a9f76898.vercel.app/', // replace with your real frontend URL
+  credentials: true
+}));
 app.use((req, res, next) => {
     res.setHeader('X-Common-Header', 'This is a common header');
     next();
